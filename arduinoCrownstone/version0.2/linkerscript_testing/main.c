9b6af89e4c2d65ed3417b39a622aff16fccf8638
Paul Wondel <0947421@hr.nl>
2019-11-28 17:33:17 +0100

    end of the day

--- arduinoCrownstone/version0.2/linkerscript_testing/main.c
+++ arduinoCrownstone/version0.2/linkerscript_testing/main.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-int main( int argc, const char* argv[] )
-{
-	printf( "\nHello World\n\n" );
+#include <iostream> 
+using namespace std; 
+  
+int main(int argc, char** argv) 
+{ 
+    cout << "You have entered " << argc 
+         << " arguments:" << "\n"; 
+  
+    for (int i = 0; i < argc; ++i) 
+        cout << argv[i] << "\n"; 
+  
+    return 0; 
 }
