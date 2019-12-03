4f43279192a009983a85678b5841c831e7fb90ec
Paul Wondel <0947421@hr.nl>
2019-12-03 17:25:12 +0100

    created new linker script, made c program, end of the day commit

--- arduinoCrownstone/version0.2/linkerscript_testing/address_testing/main.c
+++ arduinoCrownstone/version0.2/linkerscript_testing/address_testing/main.c
@@ -3,36 +3,45 @@
 static size_t func; //variable to store function address into
 
 void callback() {
-    printf("This is the arduino callback function! \n");
+	printf("This is the arduino callback function! \n");
 }
 
-void functionAdress(){
+int functionAddress(int x){
+	printf("%d\n", x);	
 }
 
 
 int main(int argc, const char* argv[]) {
    
-   int var1;
-   int *p;
+	int var1;
+	int *p;
+	
+	p = &var1;
+	
+	printf("Address of the variable: %p\n", &var1); //Prints the address of the variable
+	printf("Value of the target of the pointer: %d\n",*p); //Prints the value of the pointer
+	
+	var1 = 20;
+	
+	printf("Value of the target of the pointer: %d\n",*p); //Prints the value of the pointer
+	
+	callback();
+	printf("Address of callback function: %p\n", &callback);
+	
+	//int *ptr_func = &callback; 
+	//func = ptr_func;
+	//func = &callback;
+	
+	int (*foo)(int);
+	foo = &functionAddress;
+	(*foo)(2);
+	
+	//typedef void (*func)(); //declaring a function pointer
+	
+	void (*func)(); // declare function pointer
+	func = &callback; // assign address to pointer
+	(*func)(); // call function indirectly
+	printf("func: %p\n", func); // I need to print the type of the &callback
 
-   p = &var1;
-
-   printf("Address of the variable: %p\n", &var1); //Prints the address of the variable
-   printf("Value of the target of the pointer: %d\n",*p); //Prints the value of the pointer
-   
-   var1 = 20;
-
-   printf("Value of the target of the pointer: %d\n",*p); //Prints the value of the pointer
-
-   callback();
-   printf("Address of callback function: %p\n", &callback);
-
-   func = &callback;
-
-   // call function indirectly
-   // size_t (*func)(unsigned int, unsigned int); //declaring a function pointer
-   
-   // I need to print the type of the &callback
-
-   return 0;
+	return 0;
 }
